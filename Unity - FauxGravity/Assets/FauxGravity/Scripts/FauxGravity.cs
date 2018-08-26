638769e3bd53a8bfa11c1036572eedf6613a5838
tejpratap0 <tej.kiet@gmail.com>
2018-08-27 03:15:33 +0530

    Project Setup
    A example of Faux Gravity.

---
+++ Unity - FauxGravity/Assets/FauxGravity/Scripts/FauxGravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravity : MonoBehaviour {
+    public FauxGravityAttractor Attractor;
+    private Rigidbody rigidbody;
+    private Transform planetTransform;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        // rigidbody.constraints = RigidbodyConstraints.FreezeRotation;  // Not Working
+        rigidbody.useGravity = false;
+        planetTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attractor.Attract(planetTransform);
+    }
+}
---
+++ Unity - FauxGravity/Assets/FauxGravity/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 15;
+    private Vector3 moveDir;
+    private Rigidbody rigidbody;
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+    }
+}
